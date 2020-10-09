c61f09fec1a074e23cd1124af5f02aee66be5f5d
Dimiter Zlatkov <Dzdimi14@gmail.com>
2020-10-09 14:06:18 -0700

    Add files via upload

--- racing game/car.py
+++ racing game/car.py
@@ -11,7 +11,7 @@ class Car:
     def __init__(self, x, y, angle =90.0, length = 4, max_steering = 15, max_acceleration = 30.0):
         self.x = x
         self.y = y
-        self.position = Vector2(x, y)
+        self.position = Vector2(self.x, self.y)
         self.velocity = Vector2(0.0, 0.0)
         self.angle = angle
         self.length = length
@@ -26,47 +26,50 @@ class Car:
         self.brake_deceleration = max_acceleration
 
 
+    def reset(self):
+        self.x = 120
+        self.y = 480
+        self.position = Vector2(120, 480)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = 90.0
+        print(self.position)
+
     def accelerate(self, pressed, dt):
         
         if pressed[pygame.K_UP]:
             self.acceleration += self.acceleration_factor * dt
-
-        elif pressed[pygame.K_DOWN]:
-            self.acceleration -= self.acceleration_factor * dt
         else:
             self.acceleration = 0
+            #self.velocity -= (self.velocity / 55) #friction
 
         self.acceleration = max(-self.max_acceleration, min(self.acceleration, self.max_acceleration))
-
 
     def brake(self, pressed, dt):
-        #TODO brakes dont work, do same thing as letting go
-        if pressed[pygame.K_UP]:
-            if self.velocity.x < 0:
-                self.acceleration = self.brake_deceleration
-            else:
-                self.acceleration += 1 * dt
-        elif pressed[pygame.K_DOWN]:
-            if self.velocity.x > 0:
-                self.acceleration = -self.brake_deceleration
-            else:
-                self.acceleration -= 1 * dt
-        elif pressed[pygame.K_SPACE]:
-            if abs(self.velocity.x) > dt * self.brake_deceleration:
-                self.acceleration = copysign(self.max_acceleration, -self.velocity.x)
-            else:
-                self.acceleration = -self.velocity.x / dt
-        else:
-            if dt != 0:
-                self.acceleration = -self.velocity.x /dt
+        if pressed[pygame.K_DOWN]:
+            self.acceleration = -self.brake_deceleration
         self.acceleration = max(-self.max_acceleration, min(self.acceleration, self.max_acceleration))
+
+    def steer(self, pressed, dt):
+        if pressed[pygame.K_RIGHT]:
+            self.steering -= self.steering_rate * dt
+        elif pressed [pygame.K_LEFT]:
+            self.steering += self.steering_rate * dt
+        else:
+            self.steering = 0
+            #TODO make it more realistic with no input
+        self.steering = max(-self.max_steering, min(self.steering, self.max_steering))
+
 
     def update(self, dt):
         #velocity modifier to make car feel lighter
         mod = 1
 
-        self.velocity += (self.acceleration * dt * mod, 0) #dt - the amount of seconds passed since last frame
-        #print(self.velocity)
+        ###B R O K E N
+       # print("velocity :" + str(self.velocity))
+       # print("acceleration :" + str(self.acceleration))
+        self.velocity += (self.acceleration * dt, 0) #dt - the amount of seconds passed since last frame
+        ###B R O K E N
+        self.velocity -= self.velocity / 600  # friction
         #print(self.acceleration)
         
         if self.steering:
@@ -80,3 +83,4 @@ class Car:
         
 
 
+         
